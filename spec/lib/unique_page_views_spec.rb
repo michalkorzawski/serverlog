2ddffef34f3aef1ce00eb4acdbd7e521ffdf33cb
michalkorzawski <michalkorzawski@gmail.com>
2019-06-04 22:33:40 +0200

    add spec to UniquePageViews

--- spec/lib/unique_page_views_spec.rb
+++ spec/lib/unique_page_views_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe UniquePageViews do
   end
 
   it { expect(call).to eq expected_result }
+
+  let(:formatted_print) { described_class.new(server_log).to_s }
+  let(:formatted_output) do
+    <<~END
+    unique page views:
+    /index 6 unique views
+    /home 5 unique views
+    /help_page/1 5 unique views
+    /about 3 unique views
+    /about/2 3 unique views
+    /contact 3 unique views
+    END
+  end
+
+  it do
+    expect { formatted_print }.to output(formatted_output).to_stdout
+  end
 end
