badda017ef553893fd8857309c0dd9a82f9da19e
michalkorzawski <michalkorzawski@gmail.com>
2019-06-04 22:12:03 +0200

    add UniquePageViews class

---
+++ lib/unique_page_views.rb
@@ -0,0 +1,31 @@
+class UniquePageViews
+  def initialize(server_log)
+    @server_log = server_log
+  end
+
+  def call
+    result
+  end
+
+  private
+
+  def uniq_set
+    set = Set.new
+
+    @server_log.each.with_object(set) do |line|
+      set.add(line.split(' '))
+    end
+  end
+
+  def count_uniq_views
+    hash = Hash.new(0)
+
+    uniq_set.each.with_object(hash) do |entity_arr|
+      hash[entity_arr[0]] += 1
+    end
+  end
+
+  def result
+    @result ||= count_uniq_views.sort_by {|key, value| value}.reverse.to_h
+  end
+end
